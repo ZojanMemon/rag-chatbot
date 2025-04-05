2af7edeec5a6a5e81ca831660ffd7fb36fc0413b
AryanQureshi <themusicking151@gmail.com>
2025-04-05 19:16:27 +0500

    remove profile icon

--- app.py
+++ app.py
@@ -325,6 +325,11 @@ def main():
         }
 
 
+._ createdByLabel_g
+zau3_104. dark gzau3_91{
+display: none !important;
+}
+
         ._darkThemeShadow_gzau3_91 {
     box-shadow: 0 2px 5px 4px #9797971f, 0 0 4px 2px #0f0f151f;
     display: none important;
